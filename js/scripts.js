935fb90a3a88449222cdeba28f4cd672e6a11fe6
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-07-14 21:49:41 -0700

    change all ingredients labels to toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 //Business Logic
 
-
+function Pizza(toppings, size){
+  this.toppings = toppings;
+  this.size = size;
+  this.cost = 0;
+}
 
 
 
@@ -15,20 +19,19 @@
 function gatherFormValues(e) {
   e.preventDefault();
   
-  ingredArray = [];
-  const ingredients = document.querySelectorAll("input[name='ingredients']:checked");
-  ingredients.forEach(topping => {
-    ingredArray.push(topping.value);
+  toppingArray = [];
+  const tops = document.querySelectorAll("input[name='toppings']:checked");
+  tops.forEach(topping => {
+    toppingArray.push(topping.value);
   });
 
   const size = document.querySelector("input[name='size']:checked").value;
-  console.log(size);
   
   const pizzaForm = document.getElementById('orderForm');
   pizzaForm.reset();
 
-  let pizza = new Pizza(ingredientArray, size);
-  return pizza;
+  let pizza = new Pizza(toppingArray, size);
+  console.log(pizza);
 }
 
 
